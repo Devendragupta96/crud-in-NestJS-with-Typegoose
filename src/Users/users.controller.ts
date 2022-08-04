49ba1782c1c6a8541851a25d334d451694fc4fbf
Devendragupta96 <devendrakochartech@gmail.com>
2022-08-04 10:03:12 +0530

    updated

--- src/Users/users.controller.ts
+++ src/Users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body,Get, Post, Patch, Param, Query, Delete, NotFoundException } from '@nestjs/common';
+import { Controller, Body, Get, Post, Patch, Param, Delete } from '@nestjs/common';
 import { UpdateUserDto } from './update.dto';
 import { User } from './users.model';
 import { UsersService } from './users.service';
@@ -7,26 +7,26 @@ import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService:UsersService){}
+    constructor(private readonly usersService: UsersService) { }
 
     @Post('/create')
-    async create(@Body() user: User): Promise<User>{
+    async create(@Body() user: User): Promise<User> {
         return await this.usersService.create(user);
-        
+
     }
-    
+
     @Get()
-    async getUser():Promise<User[]|null>{
+    async getUser(): Promise<User[] | null> {
         return await this.usersService.findAll();
     }
 
     @Delete('/:_id')
-    removerUser(@Param('_id') _id: string){
+    removerUser(@Param('_id') _id: string) {
         return this.usersService.remove(_id);
     }
 
     @Patch('/:_id')
-    UpdateUser(@Param('_id') _id:string,@Body() body:UpdateUserDto){
+    UpdateUser(@Param('_id') _id: string, @Body() body: UpdateUserDto) {
         return this.usersService.update(_id, body);
     }
 }
